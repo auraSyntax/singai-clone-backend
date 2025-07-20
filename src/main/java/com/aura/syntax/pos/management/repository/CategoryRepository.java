1d386eb9a20806cdf6f46f69c4782e1af35282e3
NIJANTHAN <nijanthanruberd@gmail.com>
2025-07-20 16:04:06 +0530

    commitn1

--- src/main/java/com/aura/syntax/pos/management/repository/CategoryRepository.java
+++ src/main/java/com/aura/syntax/pos/management/repository/CategoryRepository.java
@@ -14,7 +14,7 @@ public interface CategoryRepository extends JpaRepository<Categories, Long> {
 
     @Query("SELECT NEW com.aura.syntax.pos.management.api.dto.CategoryDto(c.id,c.name,c.description,c.imageUrl,c.status,c.mainCategoryId) " +
            "FROM Categories c " +
-           "WHERE c.mainCategoryId = :mainCategoryId " +
+           "WHERE :mainCategoryId IS NULL OR c.mainCategoryId = :mainCategoryId " +
            "AND c.status = 'ACTIVE' " +
            "ORDER BY c.createdAt")
     List<CategoryDto> getAllCategories(Long mainCategoryId);
