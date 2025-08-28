7e903293e593a87c9d19d884592b1cf0a54a1594
NIJANTHAN <nijanthanruberd@gmail.com>
2025-08-28 21:41:07 +0530

    commitn1

--- src/main/java/com/aura/syntax/pos/management/config/SecurityConfig.java
+++ src/main/java/com/aura/syntax/pos/management/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://singai-pos.onrender.com"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://singai-pos.onrender.com","https://astounding-monstera-9f8e37.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
